b11e9a7e30c8c924bbb5e3829b490a270b4d07be
devopsdj <michael@blue360media.com>
2022-04-14 10:04:56 -0700

    created framwork/shopify/products folder structure to separate products from index.tsx file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
-export default function Home() {
-  const message: string = "Hello World";
+import type { InferGetStaticPropsType } from "next";
+import getAllProducts from "../framework/shopify/products/get-all-products";
 
-  return <div>{message}</div>;
+export async function getStaticProps() {
+  const products = await getAllProducts();
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 4 * 60 * 60,
+  };
+}
+
+export default function Home({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <div>{products}</div>;
 }
