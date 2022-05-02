44102776b64120005fe630c13e04b19332f1046d
devopsdj <michael@blue360media.com>
2022-05-02 09:53:15 -0700

    fetcher return type

--- framework/shopify/utils/queries/get-all-products.ts
+++ framework/shopify/utils/queries/get-all-products.ts
@@ -1,38 +1,38 @@
 const productConnection = `
 	pageInfo {
-			hasNextPage
-			hasPreviousPage
+		hasNextPage
+		hasPreviousPage
+	}
+	edges {
+		node {
+			id
+			title
+			vendor
+			handle
+			description
+			priceRange {
+				minVariantPrice {
+					amount
+					currencyCode
+				}
 			}
-			edges {
-				node {
-					id
-					title
-					vendor
-					handle
-					description
-					priceRange {
-						minVariantPrice {
-							amount
-							currencyCode
-						}
-					}
-					images(first: 1) {
-						pageInfo {
-							hasNextPage
-							hasPreviousPage
-						}
-						edges {
-							node {
-								originalSrc
-								altText
-								width
-								height
-							}
-						}
+			images(first: 1) {
+				pageInfo {
+					hasNextPage
+					hasPreviousPage
+				}
+				edges {
+					node {
+						originalSrc
+						altText
+						width
+						height
 					}
 				}
 			}
-		`;
+		}
+	}
+`;
 
 const getAllProductsQuery = `
   query getAllProducts($first: Int = 250) {
