f9c28452082d513c8b974b01c59f3d13108aa146
devopsdj <michael@blue360media.com>
2022-05-05 09:42:05 -0700

    made alias imports inside of tsconfig.json file and then recompiled by restarting server

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { InferGetStaticPropsType } from "next";
-import getAllProducts from "../framework/shopify/products/get-all-products";
+import getAllProducts from "@framework/products/get-all-products";
 
 export async function getStaticProps() {
   const products = await getAllProducts();
